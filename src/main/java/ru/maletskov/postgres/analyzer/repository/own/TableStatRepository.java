62dc4561f313f5fd3acca67a7308aa322bb4015c
Matvey Maletskov <matvei.maletskov@simbirsoft.com>
2020-11-29 16:02:03 +0300

    add query for collect statistic between dates

--- src/main/java/ru/maletskov/postgres/analyzer/repository/own/TableStatRepository.java
+++ src/main/java/ru/maletskov/postgres/analyzer/repository/own/TableStatRepository.java
@@ -10,4 +10,15 @@ public interface TableStatRepository extends JpaRepository<TableStat, Long> {
 
     @Query(value = "select * from main_stat where created > :time", nativeQuery = true)
     List<TableStat> findAllByCreated(LocalDateTime time);
+
+    @Query(value = "select schema_name, " +
+            "       table_name, " +
+            "       sum(read_val) as read_val, " +
+            "       sum(ins_val)  as ins_val, " +
+            "       sum(upd_val)  as upd_val, " +
+            "       sum(del_val)  as del_val " +
+            "from main_stat " +
+            "where created between :start and :date " +
+            "group by schema_name, table_name", nativeQuery = true)
+    List<TableStat> findAllBetweenTwoDates(LocalDateTime start, LocalDateTime end);
 }

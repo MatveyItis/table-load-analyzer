62299845c53b76583c2d131e8aa93e89dcfe1ce8
Matvey Maletskov <matvei.maletskov@simbirsoft.com>
2020-11-29 21:11:11 +0300

    fix

--- src/main/java/ru/maletskov/postgres/analyzer/repository/own/TableStatRepository.java
+++ src/main/java/ru/maletskov/postgres/analyzer/repository/own/TableStatRepository.java
@@ -11,7 +11,7 @@ public interface TableStatRepository extends JpaRepository<TableStat, Long> {
     @Query(value = "select * from main_stat where created > :time", nativeQuery = true)
     List<TableStat> findAllByCreated(LocalDateTime time);
 
-    @Query(value = "select schema_name, " +
+/*    @Query(value = "select schema_name, " +
             "       table_name, " +
             "       sum(read_val) as read_val, " +
             "       sum(ins_val)  as ins_val, " +
@@ -20,5 +20,5 @@ public interface TableStatRepository extends JpaRepository<TableStat, Long> {
             "from main_stat " +
             "where created between :start and :date " +
             "group by schema_name, table_name", nativeQuery = true)
-    List<TableStat> findAllBetweenTwoDates(LocalDateTime start, LocalDateTime end);
+    List<TableStat> findAllBetweenTwoDates(LocalDateTime start, LocalDateTime end);*/
 }
